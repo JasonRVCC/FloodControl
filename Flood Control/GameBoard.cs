c7cff35b00cd8cc667e1c9829bea909360d41d10
JasonRVCC <G00136378@W213-30.academic.raritanval.edu>
2015-09-11 13:31:28 -0400

    Work from Chapter 2

---
+++ Flood Control/GameBoard.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+//This class represents the entire board of a Flood Control game.
+//It is responsible for:
+//-Storing GamePiece objects for each square on the board.
+//-Passing update calls to individual game pieces.
+//-Randomly assigning types to game pieces.
+//-Setting and clearing the "water" flag for each game piece.
+//-Determining which pieces need to be filled with water.
+//-Returning lists of potenially scoring chains.
+
+namespace Flood_Control
+{
+    class GameBoard
+    {
+        Random rand = new Random();
+
+        public const int GameBoardWidth = 8;
+        public const int GameBoardHeight = 10;
+
+        private GamePiece[,] boardSquares = new GamePiece[GameBoardWidth, GameBoardHeight];
+
+        private List<Vector2> WaterTracker = new List<Vector2>();
+
+        //Create the Game Board
+        public GameBoard()
+        {
+            ClearBoard();
+        }
+
+        //Clear the board, change every square to an empty piece
+        public void ClearBoard()
+        {
+            for (int x = 0; x < GameBoardWidth; x++)
+                for (int y = 0; y < GameBoardHeight; y++)
+                    boardSquares[x,y] = new GamePiece("Empty");
+        }
+
+        //Rotate a game piece
+        public void RotatePiece(int x, int y, bool clockwise)
+        {
+            boardSquares[x,y].RotatePiece(clockwise);
+        }
+
+        //Get the source rectangle for a piece's current sprite
+        public Rectangle GetSourceRect(int x, int y)
+        {
+            return boardSquares[x,y].GetSourceRect();
+        }
+
+        //Get a piece's square type
+        public string GetSquare(int x, int y)
+        {
+            return boardSquares[x,y].PieceType;
+        }
+
+        //Set a piece's square type
+        public void SetSquare(int x, int y, string pieceName)
+        {
+            boardSquares[x,y].SetPiece(pieceName);
+        }
+
+        //Check if a certain side of a piece has a connector
+        public bool HasConnector(int x, int y, string direction)
+        {
+            return boardSquares[x,y].HasConnector(direction);
+        }
+
+        //Randomly set a piece's type
+        public void RandomPiece(int x, int y)
+        {
+            boardSquares[x,y].SetPiece(GamePiece.PieceTypes[rand.Next(0,
+                GamePiece.MaxPlayablePieceIndex + 1)]);
+        }
+
+        //After a scoring chain has been removed, move the above pieces downward
+        public void FillFromAbove(int x, int y)
+        {
+            int rowLookup = y - 1;
+            
+            while (rowLookup >= 0)
+            {
+                if (GetSquare(x, rowLookup) != "Empty")
+                {
+                    SetSquare(x, y, GetSquare(x, rowLookup));
+                    SetSquare(x, rowLookup, "Empty");
+                    rowLookup = -1;
+                }
+                rowLookup--;
+            }
+        }
+
+        //Generate new pieces to fill empty squares
+        public void GenerateNewPieces(bool dropSquares)
+        {
+            if (dropSquares)
+            {
+                for (int x =0; x < GameBoardWidth; x++)
+                {
+                    for (int y = GameBoardHeight - 1; y>= 0; y--)
+                    {
+                        if (GetSquare(x,y) == "Empty")
+                            FillFromAbove(x,y);
+                    }
+                }
+            }
+
+            for (int y = 0; y < GameBoardHeight; y++)
+                for (int x = 0; x < GameBoardWidth; x++)
+            {
+                if (GetSquare(x,y) == "Empty")
+                    RandomPiece(x,y);
+            }
+        }
+
+        //Reset the water
+        public void ResetWater()
+        {
+            for (int y = 0; y < GameBoardHeight; y++)
+                for (int x = 0; x < GameBoardWidth; x++)
+                    boardSquares[x,y].RemoveSuffix("W");
+        }
+
+        //Fill a piece with water
+        public void FillPiece(int x, int y)
+        {
+            boardSquares[x,y].AddSuffix("W");
+        }
+
+        //Determine which piece should be filled with water
+        public void PropagateWater(int x, int y, string fromDirection)
+        {
+            if ((y >= 0) && (y < GameBoardHeight) && (x >= 0) && (x < GameBoardWidth))
+            {
+                if (boardSquares[x,y].HasConnector(fromDirection) && 
+                    !boardSquares[x,y].Suffix.Contains("W"))
+                {
+                    FillPiece(x,y);
+                    WaterTracker.Add(new Vector2(x,y));
+                    foreach (string end in boardSquares[x,y].GetOtherEnds(fromDirection))
+                        switch (end)
+                        {
+                            case "Left":
+                                PropagateWater(x - 1, y, "Right");
+                                break;
+                            case "Right":
+                                PropagateWater(x + 1, y, "Left");
+                                break;
+                            case "Top":
+                                PropagateWater(x, y - 1, "Bottom");
+                                break;
+                            case "Bottom":
+                                PropagateWater(x, y + 1, "Top");
+                                break;
+                        }
+                }
+            }
+        }
+
+        //Check if a scoring water chain has been made
+        public List<Vector2> GetWaterChain(int y)
+        {
+            WaterTracker.Clear();
+            PropagateWater(0, y, "Left");
+            return WaterTracker;
+        }
+    }
+}
---
+++ Flood Control/GamePiece.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+//This class represents an individual pipe on the Flood Control game board.
+//It is responsible for:
+//-Identifying which sides of the piece have connectors
+//-Wheither or not the piece is filled with water.
+//-Automaticly handling rotation.
+//-Returning connector info to other pieces.
+//-Providing the sprite sheet location for the piece's current sprite. 
+
+namespace Flood_Control
+{
+    class GamePiece
+    {
+        public static string[] PieceTypes =
+        {
+            "Left,Right","Top,Bottom","Left,Top","Top,Right",
+            "Right,Bottom","Bottom,Left","Empty"
+        };
+
+        public const int PieceHeight = 40;
+        public const int PieceWidth = 40;
+
+        public const int MaxPlayablePieceIndex = 5;
+        public const int EmptyPieceIndex = 6;
+
+        private const int textureOffsetX = 1;
+        private const int textureOffsetY = 1;
+        private const int texturePaddingX = 1;
+        private const int texturePaddingY = 1;
+
+        private string pieceType = "";
+        private string pieceSuffix = "";
+
+        //Create a new piece with a type and suffix
+        public GamePiece(string type, string suffix)
+        {
+            pieceType = type;
+            pieceSuffix = suffix;
+        }
+
+        //Create a new piece with just a type
+        public GamePiece(string type)
+        {
+            pieceType = type;
+            pieceSuffix = "";
+        }
+
+        //Get Piece Type
+        public string PieceType
+        {
+            get {return pieceType;}
+        }
+
+        //Get Suffix
+        public string Suffix
+        {
+            get {return pieceSuffix;}
+        }
+
+        //Update the game piece
+        //With type and suffix
+        public void SetPiece(string type, string suffix)
+        {
+            pieceType = type;
+            pieceSuffix = suffix;
+        }
+
+        //With type only
+        public void SetPiece(string type)
+        {
+            SetPiece(type, "");
+        }
+
+        //Add a suffix
+        public void AddSuffix(string suffix)
+        {
+            if(!pieceSuffix.Contains(suffix))
+                pieceSuffix += suffix;
+        }
+
+        //Remove the suffix (suffix must be passed)
+        public void RemoveSuffix(string suffix)
+        {
+            pieceSuffix = pieceSuffix.Replace(suffix, "");
+        }
+
+        //Rotate the game piece
+        public void RotatePiece(bool clockwise)
+        {
+            switch (pieceType)
+            {
+                case "Left,Right":
+                    pieceType = "Top,Bottom";
+                    break;
+                case "Top,Bottom":
+                    pieceType = "Left,Right";
+                    break;
+                case "Left,Top":
+                    if(clockwise)
+                        pieceType = "Top,Right";
+                    else
+                        pieceType = "Bottom,Left";
+                    break;
+                case "Top,Right":
+                    if(clockwise)
+                        pieceType = "Right,Bottom";
+                    else
+                        pieceType = "Left,Top";
+                    break;
+                case "Right,Bottom":
+                    if(clockwise)
+                        pieceType = "Bottom,Left";
+                    else
+                        pieceType = "Top,Right";
+                    break;
+                case "Bottom,Left":
+                    if(clockwise)
+                        pieceType = "Left,Top";
+                    else
+                        pieceType = "Right,Bottom";
+                    break;
+                case "Empty":
+                    break;
+            }
+        }
+
+        //Get connector locations
+        public string[] GetOtherEnds(string startingEnd)
+        {
+            List<string> opposites = new List<string>();
+
+            foreach (string end in pieceType.Split(','))
+            {
+                if (end != startingEnd)
+                    opposites.Add(end);
+            }
+            return opposites.ToArray();
+        }
+
+        //Check if a certain side of the piece has a connector 
+        public bool HasConnector(string direction)
+        {
+            return pieceType.Contains(direction);
+        }
+
+        //Get the source rectangle for the piece's current sprite
+        public Rectangle GetSourceRect()
+        {
+            int x = textureOffsetX;
+            int y = textureOffsetY;
+
+            if (pieceSuffix.Contains("W"))
+                x += PieceWidth + texturePaddingX;
+
+            y += (Array.IndexOf(PieceTypes, pieceType) * (PieceHeight + texturePaddingY));
+
+            return new Rectangle(x, y, PieceWidth, PieceHeight);
+        }
+    }
+}
